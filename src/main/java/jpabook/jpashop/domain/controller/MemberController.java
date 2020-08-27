1c618be275c247aad3e97b9ffbcf5ad2b3ab6f3f
wonseok <wonseok5893@naver.com>
2020-08-27 23:56:36 +0900

    회원 목록 완료

--- src/main/java/jpabook/jpashop/domain/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/domain/controller/MemberController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.jws.WebParam;
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -35,5 +37,13 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String memberList(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return  "members/memberList";
+
     }
 }
